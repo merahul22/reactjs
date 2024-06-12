4cda3ac0c56c7050770c14c29296c7ae665c1557
Rahul chourasiya <130483145+merahul22@users.noreply.github.com>
2024-06-12 19:59:56 +0000

    auth service introduced using appwrite

---
+++ BlogProject/src/appwrite/auth.js
@@ -0,0 +1,49 @@
+import { Client, Account, ID } from "appwrite";
+
+
+export class Authservice{
+  client=new Client()
+ account;
+   constructor(){
+    this.client
+         .setEndpoint(conf.appwriteUrl)
+         .setProject(conf.appwriteProjectID);
+      this.acccont=new Account(this.client)
+   }
+ async createAccount({email,password,name}){
+               try {
+              const useraccount= await this.account.create(ID.unique(),email,password,name)
+if(useraccount) return this.login({email,password})
+    else return useraccount
+               } catch (error) {
+                throw error
+               }
+   }
+
+   async login({email,password}){
+    try {
+        return this.account.createEmailPasswordSession(email,password) 
+    } catch (error) {
+        throw error
+    }
+   }
+
+   async currentUser(){
+  try {
+     return await this.acccont.get()
+  } catch (error) {
+    throw error
+  }
+  return null
+   }
+   async logout(){
+    try {
+       return await this.acccont.deleteSessions() //deletes sessions for all browsers
+    } catch (error) {
+        throw error
+    }
+
+   }
+}
+
+ export const authservice=new Authservice()
---
+++ BlogProject/src/conf/conf.js
@@ -0,0 +1,7 @@
+const conf={
+    appwriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectID:String(import.meta.env.VITE_APPWRITE_PROJECTID),
+    appwriteBucket:String(import.meta.env.VITE_APPWRITE_BUCKET),
+    appwriteDatabaseID:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionID:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID)
+}
